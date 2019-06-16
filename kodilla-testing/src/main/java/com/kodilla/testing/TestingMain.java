d7b80dd7373f9b2fe3ae70a47114bc743ba1afa5
krzysztofplociennik <krzysztofplociennik6@gmail.com>
2019-06-16 12:20:54 +0100

    Test jednostkowy (6.2)

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,26 @@
 package com.kodilla.testing;
+import com.kodilla.testing.calculator.Calculator;
+
 
 public class TestingMain {
     public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        //System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+
+        Calculator test1 = new Calculator();
+
+        int sum = test1.add(4, 6);
+        int dif = test1.substract(10, 2);
+
+        if(sum==(10)) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        if(dif==(8)) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
     }
 }
