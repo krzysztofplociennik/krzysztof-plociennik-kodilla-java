557346c65b644fab0e231bc61cae838a932ac64a
krzysztofplociennik <krzysztofplociennik6@gmail.com>
2019-06-30 08:48:26 +0100

    try! catch! finally! (8.2)

---
+++ kodilla-exception/src/main/java/com/kodilla/exception/io/FileReader.java
@@ -0,0 +1,34 @@
+package com.kodilla.exception.io;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
+public class FileReader {
+
+    public void readFile() {
+        ClassLoader classLoader = getClass().getClassLoader();
+        File file = new File(classLoader.getResource("file/names.txt").getFile());
+
+        //System.out.println(file.getPath());
+
+        Path path = Paths.get(file.getPath());
+        //Path path = Paths.get("file/nameas.txt");
+
+        try {
+            Stream<String> fileLines = Files.lines(path);
+            fileLines.forEach(System.out::println);
+
+        } catch (IOException e) {
+            System.out.println("Something went wrong! Error: " + e);
+
+        } finally {
+            System.out.println("I'm gonna be here always!");
+        }
+
+
+    }
+}
